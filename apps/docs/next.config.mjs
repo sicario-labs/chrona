b423818114604efa8a4d36ec65453a99ea162a61
chrona <dev@chrona.local>
2026-08-22 22:26:59 +0100

    fix: import createMDX as default export in next.config.mjs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,4 +1,4 @@
-import { createMDX } from 'fumadocs-mdx/config';
+import createMDX from 'fumadocs-mdx/config';
 const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = { reactStrictMode: true };
